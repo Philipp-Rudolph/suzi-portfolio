f5b1b25d60dc7a329e060612c92be9bd676f2b48
Philipp-Rudolph <83408627+Philipp-Rudolph@users.noreply.github.com>
2025-04-21 11:06:37 +0000

    Deploying to gh-pages from @ Philipp-Rudolph/suzi-portfolio@4429f220bb21ffbe5dbe7710707f99542a058915 🚀

--- _nuxt/BchutocS.js
+++
@@ -1,1 +0,0 @@
-import{a1 as a,a2 as t}from"./C33_9FJV.js";const i=a((e,o)=>t("/login"));export{i as default};
---
+++ _nuxt/DoPHVqY8.js
@@ -0,0 +1,1 @@
+import{d as e,c as o,i as a}from"./BJeGHby7.js";const s=e({__name:"dashboard",setup(r){return(t,n)=>(a(),o("h1",null,"Welcome to your dashboard"))}});export{s as default};
